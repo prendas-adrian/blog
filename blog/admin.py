565cead59b0c59409e3d6bc4e049db8314129e2a
Adrian Prendas <prendas.adrian@proton.me>
2026-08-23 16:17:10 -0600

    list_display, ordering, list_filter by tags, and removed prepopulated slug

--- blog/admin.py
+++ blog/admin.py
@@ -5,8 +5,10 @@ from blog.models import Post, Tag
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
     # Ensure slug is present in the form so the admin JS can prepopulate it
-    fields = ("user",'title',"tags", 'slug', 'description', 'content', 'image')
-    prepopulated_fields = {'slug': ('title',)}
+    fields = ("user",'title',"tags", 'description', 'content', 'image')
+    list_display = ("title", "user", "created_at", "updated_at")
+    ordering = [("-updated_at")]
+    list_filter = [("tags")]
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Tag)
